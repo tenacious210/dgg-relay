57734408f39ef0c1a6d718700e239d95cbc4cf97
tenacious210 <tenacious210@gmail.com>
2022-02-18 17:19:09 -0500

    Getting closer but not working yet

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ from dggbot.message import Message
 from dggbot import DGGBot
 from os import getenv
 from time import sleep
+from threading import Thread
+from asyncio import run, get_running_loop, create_task
 import discord
+
 
 info = {}
 discord_bot = discord.Client()
 
-dgg_bot = DGGBot(getenv("DGG_AUTH"), username="TenaReturns", owner="Fritz")
-
-
-async def fwd_to_disc(dgg_msg: Message):
-    await info["relay_channel"].send(f"{dgg_msg.nick} said: {dgg_msg.data}")
+dgg_bot = DGGBot(auth_token=getenv("DGG_AUTH"), username="TenaReturns", owner="Fritz")
+dgg_thread = Thread(target=dgg_bot.run)
 
 
 @discord_bot.event
 async def on_ready():
     info["relay_channel"] = discord_bot.get_channel(944252065515962468)
     print(f'Forwarding messages to {info["relay_channel"]}')
+    info["disc_loop"] = get_running_loop()
+    dgg_thread.start()
 
 
 @discord_bot.event
@@ -29,23 +31,11 @@ async def on_message(disc_msg):
     await info["relay_channel"].send("Message received")
 
 
-@dgg_bot.event("on_join")
-def on_dgg_join(dgg_msg):
-    print(dgg_msg)
-
-
-@dgg_bot.event("on_mention")
-def on_dgg_mention(dgg_msg):
-    fwd_to_disc(dgg_msg)
-
-
 @dgg_bot.event("on_msg")
 def on_dgg_message(dgg_msg):
-    print(dgg_msg)
-    fwd_to_disc(dgg_msg)
+    info["disc_loop"].create_task(
+        info["relay_channel"].send(f"{dgg_msg.nick} said: {dgg_msg.data}")
+    )
 
 
 discord_bot.run(getenv("DISC_AUTH"))
-while True:
-    dgg_bot.run()
-    sleep(600)
