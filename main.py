e9da1837671c9da1a2884258690bc5c9a2e5479e
tenacious210 <tenacious210@gmail.com>
2022-02-19 00:18:09 -0500

    Runs slow, but works

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from dggbot import DGGBot
 from os import getenv
 from time import sleep
 from threading import Thread
-from asyncio import run, get_running_loop, create_task
+from asyncio import get_running_loop, create_task
+from pathlib import Path
+import json
 import discord
 
-
-info = {}
 discord_bot = discord.Client()
+
+with Path(__file__).with_name("config.json").open("r") as config_file:
+    config = json.loads(config_file.read())
+
 
 dgg_bot = DGGBot(auth_token=getenv("DGG_AUTH"), username="TenaReturns", owner="Fritz")
 dgg_thread = Thread(target=dgg_bot.run)
@@ -16,26 +20,35 @@ dgg_thread = Thread(target=dgg_bot.run)
 
 @discord_bot.event
 async def on_ready():
-    info["relay_channel"] = discord_bot.get_channel(944252065515962468)
-    print(f'Forwarding messages to {info["relay_channel"]}')
-    info["disc_loop"] = get_running_loop()
+    discord_bot.relay_channel = discord_bot.get_channel(944252065515962468)
+    print(f"Forwarding messages to {discord_bot.relay_channel}")
+    discord_bot.disc_loop = get_running_loop()
+    await discord_bot.relay_channel.send("Connecting to DGG")
     dgg_thread.start()
 
 
 @discord_bot.event
 async def on_message(disc_msg):
     if (disc_msg.author == discord_bot.user) or (
-        disc_msg.channel != info["relay_channel"]
+        disc_msg.channel != discord_bot.relay_channel
     ):
         return
-    await info["relay_channel"].send("Message received")
+    dgg_bot.send(disc_msg.content)
+
+
+@dgg_bot.event("on_mention")
+def on_dgg_mention(dgg_msg):
+    discord_bot.disc_loop.create_task(
+        discord_bot.relay_channel.send(f"{dgg_msg.nick} said: {dgg_msg.data}")
+    )
 
 
 @dgg_bot.event("on_msg")
 def on_dgg_message(dgg_msg):
-    info["disc_loop"].create_task(
-        info["relay_channel"].send(f"{dgg_msg.nick} said: {dgg_msg.data}")
-    )
+    if dgg_msg.nick in config["whitelist"]:
+        discord_bot.disc_loop.create_task(
+            discord_bot.relay_channel.send(f"{dgg_msg.nick} said: {dgg_msg.data}")
+        )
 
 
 discord_bot.run(getenv("DISC_AUTH"))
