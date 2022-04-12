8baa2942e1d057087eb4b0a39b5cbb373e01d59a
tenacious210 <tenacious210@gmail.com>
2022-04-11 20:53:14 -0400

    Rework started

--- main.py
+++ main.py
@@ -1,139 +1,84 @@
+from discord import Option, OptionChoice, Bot
 from dggbot import DGGBot, Message
+from discord.ext.commands import Context, has_permissions, has_role
 from os import getenv
 from threading import Thread
-from asyncio import get_running_loop, create_task
+from asyncio import get_running_loop
 from pathlib import Path
 from queue import Queue
-from discord import Option, OptionChoice, Emoji
 from time import sleep
-import logging
 import json
-import discord
 import re
 
 
-class CustomBot(DGGBot):
-    def __init__(
-        self,
-        auth_token,
-        emotes,
-        whitelist,
-        filter_level="whitelist",
-        username="tena_",
-        owner="tena_",
-        prefix="!",
-    ):
-        super().__init__(
-            auth_token=auth_token, username=username, prefix=prefix, owner=owner
-        )
-        self.username = username
-        self.msg_queue = Queue()
-        self.emotes = emotes
-        self.whitelist = whitelist
-        self.filter_level = filter_level
-
-    def _on_error(self, ws, error):
-        logging.error(error)
-        dgg_bot.msg_queue.put(f'DGG bot raised an error: "{error}"')
-
-    def run_forever(self):
-        while True:
-            self.run()
-            sleep(5)
-
-
-discord_bot = discord.Bot()
+discord_bot = Bot()
+dgg_bot = DGGBot(None)
+dgg_msg_queue = Queue()
 
 config_path = Path(__file__).with_name("config.json")
 with config_path.open("r") as config_file:
     config = json.loads(config_file.read())
 
+nicks, phrases, emotes = config["nicks"], config["phrases"], config["emotes"]
 
-dgg_bot = CustomBot(
-    getenv("DGG_AUTH"), emotes=config["emotes"], whitelist=config["whitelist"]
-)
+
+def run_dgg_bot():
+    while True:
+        dgg_bot.run()
+        sleep(5)
 
 
 def parse_dgg_queue():
     while True:
-        discord_bot.disc_loop.create_task(
-            discord_bot.relay_channel.send(dgg_bot.msg_queue.get())
-        )
+        sleep(5)
 
 
-dgg_thread = Thread(target=dgg_bot.run_forever)
-parse_dgg_thread = Thread(target=parse_dgg_queue)
+dgg_thread = Thread(target=run_dgg_bot)
+parse_dgg_queue_thread = Thread(target=parse_dgg_queue)
 
 
 def save_config():
-    to_json = {"whitelist": dgg_bot.whitelist, "emotes": dgg_bot.emotes}
+    to_json = {"nicks": nicks, "phrases": phrases, "emotes": emotes}
     with config_path.open("w") as config_file:
         json.dump(to_json, config_file, indent=2)
 
 
-def dgg_to_disc(msg: str):
-    for dgg_emote, disc_emote in dgg_bot.emotes.items():
-        msg = re.sub(rf"\b{dgg_emote}\b", disc_emote, msg)
-    msg = re.sub("[*_`]", r"\\\g<0>", msg)
-    if "nsfw" in msg:
-        msg = f"||{msg}||"
-    return msg
+def dgg_to_disc(msg: Message):
+    for dgg_emote, disc_emote in emotes.items():
+        data = re.sub(rf"\b{dgg_emote}\b", disc_emote, msg.data)
+    data = re.sub("[*_`]", r"\\\g<0>", data)
+    if "nsfw" in data:
+        data = f"NSFW ||{data}||"
+    return f"{msg.nick}: {data}"
 
 
 @discord_bot.slash_command(
-    guild_ids=[889845466915819551],
+    guild_ids=discord_bot.guilds,
     name="addemote",
-    description="Adds a phrase which gets translated from DGG emote to Discord emote",
+    description="Add or modify emotes",
 )
+@has_role("dgg-relay-mod")
 async def addemote(
-    ctx,
+    ctx: Context,
     dgg_version: Option(str, "The emote as used in DGG"),
     discord_version: Option(str, "The emote as used in Discord"),
 ):
     if (not dgg_version) or (not discord_version):
         ctx.respond("One of the parameters was invalid.")
         return
-    dgg_bot.emotes[dgg_version] = discord_version
+    emotes[dgg_version] = discord_version
     save_config()
-    await ctx.respond(f"{dgg_version} : {discord_version}", ephemeral=True)
+    await ctx.respond(f"Translating {dgg_version} to {discord_version}", ephemeral=True)
 
 
 @discord_bot.slash_command(
-    guild_ids=[889845466915819551],
-    name="filter",
-    description="Change the level of the DGG bot's filter.",
+    guild_ids=discord_bot.guilds,
+    name="relay",
+    description="Add or remove a DGG user whose messages get forwarded to this server",
 )
-async def filter(
-    ctx,
-    level: Option(
-        str,
-        "The level to change the DGG bot filter to",
-        required=True,
-        default=None,
-        choices=(
-            OptionChoice(name="Mentions only", value="mention"),
-            OptionChoice(name="Whitelist only", value="whitelist"),
-            OptionChoice(name="No filter", value="off"),
-        ),
-    ),
-):
-    if level in ("mention", "whitelist", "off"):
-        dgg_bot.filter_level = level
-        response = f'Changed the filter level to "{level}"'
-    elif level == None:
-        response = f'The filter level is "{dgg_bot.filter_level}"'
-    else:
-        response = f'Invalid filter level: "{level}"'
-    await ctx.respond(response)
-
-
-@discord_bot.slash_command(
-    guild_ids=[889845466915819551],
-    name="whitelist",
-    description="Modify the whitelist",
-)
-async def whitelist(
-    ctx,
+@has_role("dgg-relay-mod")
+async def relay(
+    ctx: Context,
     mode: Option(
         str,
         "Choose add or remove mode",
@@ -143,22 +88,34 @@ async def whitelist(
             OptionChoice(name="remove", value="remove"),
         ),
     ),
-    user: Option(
+    dgg_username: Option(
         str,
-        "The user to add/remove from the whitelist",
+        "The DGG username to add/remove",
         required=True,
     ),
 ):
-    if mode in ("add", "remove") and user:
+    if mode in ("add", "remove") and dgg_username:
+        guild = ctx.guild.id
         if mode == "add":
-            dgg_bot.whitelist.append(user)
-            response = f'"{user}" was added to the whitelist.'
-        elif mode == "remove" and user in dgg_bot.whitelist:
-            dgg_bot.whitelist.remove(user)
-            response = f'"{user}" was removed from the whitelist.'
-        elif mode == "remove" and user not in dgg_bot.whitelist:
-            await ctx.respond(f'"{user}" was not found in the whitelist.')
+            if not nicks[dgg_username]:
+                nicks[dgg_username] = []
+            if guild not in nicks[dgg_username]:
+                nicks[dgg_username].append(guild)
+                response = (
+                    f"Messages from '{dgg_username}' will be relayed to this server."
+                )
+            else:
+                response = f"Messages from '{dgg_username}' are already being relayed to this server."
+        elif mode == "remove" and guild in nicks[dgg_username]:
+            nicks[dgg_username].remove(dgg_username)
+            response = f"No longer relaying messages from '{dgg_username}'"
+        elif mode == "remove" and guild not in nicks[dgg_username]:
+            await ctx.respond(
+                f"Messages from {dgg_username} aren't being relayed to this server."
+            )
             return
+        if not nicks[dgg_username]:
+            nicks.pop(dgg_username)
         save_config()
         await ctx.respond(response)
     else:
@@ -167,40 +124,24 @@ async def whitelist(
 
 @discord_bot.event
 async def on_ready():
-    discord_bot.relay_channel = discord_bot.get_channel(944252065515962468)
-    print(f"Forwarding messages to {discord_bot.relay_channel}")
+    discord_bot.relay_channels = []
+    for guild in discord_bot.guilds:
+        for channel in guild.channels:
+            print(channel.name)
+            if channel.name == "dgg-relay" and channel.nsfw:
+                discord_bot.relay_channels.append(channel)
+    print(f"Forwarding messages to {discord_bot.relay_channels}")
     discord_bot.disc_loop = get_running_loop()
-    await discord_bot.relay_channel.send(f"Connecting to DGG as {dgg_bot.username}")
+    for channel in discord_bot.relay_channels:
+        await channel.send(f"Connecting to DGG")
     dgg_thread.start()
-    parse_dgg_thread.start()
-
-
-@discord_bot.event
-async def on_message(disc_msg):
-    if (disc_msg.author == discord_bot.user) or (
-        disc_msg.channel != discord_bot.relay_channel
-    ):
-        return
-    dgg_bot.send(disc_msg.content)
+    parse_dgg_queue_thread.start()
 
 
 @dgg_bot.event("on_msg")
 def on_dgg_message(dgg_msg):
-    if dgg_msg.nick == dgg_bot.username:
-        prefix = "S"
-    elif dgg_bot.username.lower() in dgg_msg.data.lower():
-        prefix = "M"
-    elif dgg_bot.filter_level == "whitelist" and dgg_msg.nick in dgg_bot.whitelist:
-        prefix = "WL"
-    elif dgg_bot.filter_level == "off":
-        prefix = "NF"
-    else:
-        prefix = None
-
-    if prefix:
-        dgg_bot.msg_queue.put(
-            f"**({prefix}) {dgg_msg.nick}:** {dgg_to_disc(dgg_msg.data)}"
-        )
+    dgg_msg_queue.put(dgg_msg)
 
 
-discord_bot.run(getenv("DISC_AUTH"))
+if __name__ == "__main__":
+    discord_bot.run(getenv("DISC_AUTH"))
