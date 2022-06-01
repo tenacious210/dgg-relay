9d525c425212f712a117d8898ac0bf6c14c192d3
tenacious210 <tenacious210@gmail.com>
2022-06-01 10:08:05 -0400

    Added basic logging

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ from asyncio import get_running_loop
 from pathlib import Path
 from queue import Queue
 from time import sleep
+from sys import stdout
+import logging
 import tldextract
 import requests
 import json
 import re
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(name)s:%(levelname)s: %(message)s",
+    handlers=[logging.FileHandler("logs.log"), logging.StreamHandler(stdout)],
+)
 intents = Intents.default()
 intents.members = True
 
@@ -30,6 +37,7 @@ modes = {int(k): v for k, v in config["modes"].items()}
 
 def dgg_to_disc(dgg_nick: str, dgg_txt: str):
     """Converts DGG emotes/links to Discord ones"""
+    logging.debug(f"dgg_to_disc input: {dgg_nick}: {dgg_txt}")
     dgg_txt_split = [dgg_txt]
     if link_search := set(re.findall(r"\b(\S+\.\S+)\b", dgg_txt)):
         for link in link_search:
@@ -50,6 +58,7 @@ def dgg_to_disc(dgg_nick: str, dgg_txt: str):
     dgg_nick = re.sub("[*_`|]", r"\\\g<0>", dgg_nick)
     if any([tag in disc_txt.lower() for tag in ("nsfw", "nsfl")]):
         disc_txt = f"||{disc_txt}||"
+    logging.debug(f"dgg_to_disc output: **{dgg_nick}:** {disc_txt}")
     return f"**{dgg_nick}:** {disc_txt}"
 
 
@@ -66,8 +75,12 @@ def save_config():
 
 def run_dgg_bot():
     while True:
-        dgg_bot.run()
-        sleep(5)
+        try:
+            logging.info("Starting DGG bot")
+            dgg_bot.run()
+        except ConnectionResetError:
+            logging.info("Connection reset, restarting DGG bot")
+            sleep(1)
 
 
 def parse_dgg_queue():
@@ -86,8 +99,9 @@ def parse_dgg_queue():
                         discord_bot.disc_loop.create_task(
                             channel.send(dgg_to_disc(msg.nick, msg.data))
                         )
+                    logging.debug(f"Relayed to channel {channel}")
                 else:
-                    print(f"Channel {channel_id} wasn't found")
+                    logging.warning(f"Channel {channel_id} wasn't found")
         for phrase in phrases.keys():
             lower_phrase = phrase.lower()
             if re.search(rf"\b{lower_phrase}\b", msg.data.lower()):
@@ -101,8 +115,9 @@ def parse_dgg_queue():
                             discord_bot.disc_loop.create_task(
                                 user.send(dgg_to_disc(msg.nick, msg.data))
                             )
+                        logging.debug(f"Relayed to user {user}")
                     else:
-                        print(f"User {user_id} wasn't found")
+                        logging.warning(f"User {user_id} wasn't found")
 
 
 dgg_thread = Thread(target=run_dgg_bot)
@@ -122,6 +137,7 @@ async def addemote(
             return
         emotes[dgg_version] = discord_version
         save_config()
+        logging.info(f"Emote {dgg_version} added as {discord_version}")
         await ctx.respond(f"Translating {dgg_version} to {discord_version}")
     else:
         await ctx.respond("Please contact tena#5751 to modify emotes", ephemeral=True)
@@ -160,8 +176,12 @@ async def relay(
         if mode == "add":
             if dgg_username not in nicks:
                 nicks[dgg_username] = []
+                logging.info(f'Added new relay list "{dgg_username}"')
             if relay_channel not in nicks[dgg_username]:
                 nicks[dgg_username].append(relay_channel)
+                logging.info(
+                    f'Added relay "{dgg_username}" for server "{ctx.guild.name}"'
+                )
                 response = (
                     f"Messages from '{dgg_username}' will be relayed to this server."
                 )
@@ -169,6 +189,9 @@ async def relay(
                 response = f"Messages from '{dgg_username}' are already being relayed to this server."
         elif mode == "remove" and relay_channel in nicks[dgg_username]:
             nicks[dgg_username].remove(relay_channel)
+            logging.info(
+                f'Removed relay "{dgg_username}" from server "{ctx.guild.name}"'
+            )
             response = f"No longer relaying messages from '{dgg_username}'"
         elif mode == "remove" and relay_channel not in nicks[dgg_username]:
             await ctx.respond(
@@ -177,6 +200,7 @@ async def relay(
             return
         if not nicks[dgg_username]:
             nicks.pop(dgg_username)
+            logging.info(f'Removed empty relay list for "{dgg_username}"')
         save_config()
         await ctx.respond(response)
     else:
@@ -207,17 +231,21 @@ async def phrase(
     if mode in ("add", "remove") and phrase:
         disc_user = ctx.author.id
         if disc_user not in modes.keys():
-            modes[disc_user] = "auto"
+            modes[disc_user] = "on"
+            logging.info(f"Added new user {disc_user} to modes list")
         if mode == "add":
             if phrase not in phrases:
                 phrases[phrase] = []
+                logging.info(f'Added new phrase list for "{phrase}"')
             if disc_user not in phrases[phrase]:
                 phrases[phrase].append(disc_user)
+                logging.info(f'Appended "{disc_user}" to phrase list "{phrase}"')
                 response = f"Mentions of '{phrase}' will be messaged to you."
             else:
                 response = f"Mentions of '{phrase}' are already being messaged to you."
         elif mode == "remove" and disc_user in phrases[phrase]:
             phrases[phrase].remove(disc_user)
+            logging.info(f'Removed "{disc_user}" from phrase list "{phrase}"')
             response = f"No longer relaying mentions of '{phrase}'"
         elif mode == "remove" and disc_user not in phrases[phrase]:
             await ctx.respond(
@@ -227,6 +255,7 @@ async def phrase(
             return
         if not phrases[phrase]:
             phrases.pop(phrase)
+            logging.info(f'Removed empty phrase list "{phrase}"')
         save_config()
         await ctx.respond(response, ephemeral=True)
     else:
@@ -235,7 +264,7 @@ async def phrase(
 
 @discord_bot.slash_command(
     name="phrasemode",
-    description="Enable or disable mentions forwarded through DMs. Default is auto (detects chat presence).",
+    description="Enable or disable mentions forwarded through DMs. Default is 'on'",
 )
 async def phrasemode(
     ctx: Context,
@@ -252,13 +281,8 @@ async def phrasemode(
     if mode in ("on", "off", "auto"):
         modes[ctx.author.id] = mode
         save_config()
-        await ctx.respond(f"Mention mode set to {mode}", ephemeral=True)
-    elif mode == "":
-        if ctx.author.id not in modes.keys():
-            modes[ctx.author.id] = "auto"
-        await ctx.respond(
-            f"Your mention mode is '{modes[ctx.author.id]}", ephemeral=True
-        )
+        logging.info(f'Phrase mode for "{ctx.author.id}" set to "{mode}"')
+        await ctx.respond(f"Phrase mode set to {mode}", ephemeral=True)
     else:
         await ctx.respond("Invalid parameter", ephemeral=True)
 
@@ -339,6 +363,7 @@ async def mentions(
 
 @relay.error
 async def relay_error(ctx, error):
+    logging.error(f'Error: "{error}"')
     await ctx.send(f"Error: {error}")
 
 
@@ -347,7 +372,6 @@ async def on_ready():
     discord_bot.disc_loop = get_running_loop()
     dgg_thread.start()
     parse_dgg_queue_thread.start()
-    print("Starting")
 
 
 @dgg_bot.event("on_msg")
@@ -356,4 +380,5 @@ def on_dgg_message(dgg_msg):
 
 
 if __name__ == "__main__":
+    logging.info("Starting Discord bot")
     discord_bot.run(getenv("DISC_AUTH"))
