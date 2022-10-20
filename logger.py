95996f1f780bfa16c83e3b1672c57cf250728499
tenacious210 <tenacious210@gmail.com>
2022-10-20 19:27:49 -0400

    Fix logger

--- logger.py
+++ logger.py
@@ -5,10 +5,10 @@ import sys
 
 log_format = "[%(asctime)s] %(name)s:%(levelname)s: %(message)s"
 log_file_handler = RotatingFileHandler("logs.log", maxBytes=250000000, backupCount=1)
-logging.basicConfig(level=logging.DEBUG, format=log_format, handlers=[log_file_handler])
+logging.basicConfig(level=logging.INFO, format=log_format, handlers=[log_file_handler])
 logger = logging.getLogger("dgg_relay")
 log_stream_handler = StreamHandler(sys.stdout)
-log_stream_handler.setLevel(logging.INFO)
+log_stream_handler.setLevel(logging.DEBUG)
 log_formatter = logging.Formatter(log_format)
 log_stream_handler.setFormatter(log_formatter)
 logger.addHandler(log_stream_handler)
