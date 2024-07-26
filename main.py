1a263855a39bcdecfcaaba4daf11ee22e1f4ed95
tenacious210 <tenacious210@gmail.com>
2024-07-26 10:05:14 -0400

    members intent needed for whispers

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ logging.getLogger("websocket").disabled = True
 
 class CustomDiscBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="/", intents=Intents.default())
+        intents = Intents.default()
+        intents.members = True
+        super().__init__(command_prefix="/", intents=intents)
         self.read_cfg()
         self.msg_queue = Queue()
         self.dgg_chat = DGGChat()
